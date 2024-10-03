e5afdffd3cb4261c3c0b8adea0eff24869adb33a
Garfield1337 <salkovadarya2002@gmail.com>
2024-10-03 19:26:42 +0300

    pds

--- hw.py
+++ hw.py
@@ -1,5 +1,6 @@
-x = input("Введите имя: ")
-if x = 'Алексей':
-    print ('Access denied')
-else
-print (f'Здравствуйте, {x}')
+x = input("Введите имя...\n").lower()
+aleksei_list = ['alex', 'alexei', 'alekesi', 'леха лепеха']
+if any(leha in x for leha in aleksei_list):
+    print (f'Выйдите из команты, {x.title()}')
+else:
+    print (f'Здравствуйте, {x.title()}')
